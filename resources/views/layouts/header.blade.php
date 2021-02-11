c70b7595b10c87af59ebf6a5bbc6e521b99e3cfb
FraOliv <olivotti.francesco@gmail.com>
2021-02-11 15:31:35 +0100

    adeed header template and products page (incomplete)

---
+++ resources/views/layouts/header.blade.php
@@ -0,0 +1,7 @@
+<nav class="links">
+      <img class="d-flex" src="{{asset ('img/marchio-sito.png')}}" alt="">
+      
+                <a href="/">Homepage</a>
+                <a href="products">Products</a>
+                <a href="info">Info</a>
+            </nav>
--- webpack.mix.js
+++ webpack.mix.js
@@ -13,3 +13,10 @@ const mix = require('laravel-mix');
 
 mix.js('resources/js/app.js', 'public/js')
     .sass('resources/sass/app.scss', 'public/css');
+
+    //Solo quando abbiamo immagini che non devono essere dinamiche (un img dinamica e' quella della foto profilo ad esempio), solo quelle statiche che l utente non deve poter modificare
+mix.copyDirectory('resources/img', 'public/img');
+//IMPORTANTE LE IMG dentro resources le caricheremo sempre con la sintassi {{ asset ('img/percorso')}}
+
+
+//Per immagini dinamiche vanno nella directory storage, quindi nel file storage vedi doc a riguardo

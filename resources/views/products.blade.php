82386bcf22d4717a935a88b70526e6794ceb4e5d
FraOliv <olivotti.francesco@gmail.com>
2021-02-11 17:10:10 +0100

    used foreach iterations

--- resources/views/products.blade.php
+++ resources/views/products.blade.php
@@ -1,13 +1,31 @@
+@extends('layouts.layout')
 
- @include('layouts/header')
+@section('main_content')
+    
+<main>
+    @foreach ($data as $dato)
+    @if ($loop->first)
+    <h2>LE LUNGHE</h2>
+    @endif
+    @if ($loop->iteration === 6)
+    <h2>LE CORTE</h2>
+    @endif
+     @if ($loop->last)
+      <h2> LE CORTISSIME</h2>
+    @endif
 
 
-        @foreach ($data as $dato)
+<div class="card">
 
-        <img src=" {{ $dato['src-h'] }}" alt="">
-        <h4>{{$dato['titolo']}}</h4>
+        <img src=" {{ $dato['src'] }}" alt="">
 
-  
 
-        @endforeach
-        
+</div>
+
+
+    @endforeach
+    
+    
+</main>
+@endsection
+

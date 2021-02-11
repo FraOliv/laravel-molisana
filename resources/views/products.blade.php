32310f04348cb04a472e044460d0ee3e10f34aed
FraOliv <olivotti.francesco@gmail.com>
2021-02-11 16:05:00 +0100

    test

--- resources/views/products.blade.php
+++ resources/views/products.blade.php
@@ -1,1 +1,13 @@
-<h1>test products</h1>
+
+ @include('layouts/header')
+
+
+        @foreach ($data as $dato)
+
+        <img src=" {{ $dato['src-h'] }}" alt="">
+        <h4>{{$dato['titolo']}}</h4>
+
+  
+
+        @endforeach
+        
